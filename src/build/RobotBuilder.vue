df100c0e6c0c192a738e96074939aa2c28e5de13
hkirfan <hkirfan@hsc.wvu.edu>
2019-09-12 10:31:31 -0400

    Communicating with parent component. More Child-parent interactions

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,44 +1,76 @@
 <template>
   <div class="content">
+    <div class="preview">
+      <CollapsibleSection>
+        <div class="preview-content">
+          <div class="top-row">
+            <img :src="selectedRobot.head.src" />
+          </div>
+          <div class="middle-row">
+            <img :src="selectedRobot.leftArm.src" class="rotate-left" />
+            <img :src="selectedRobot.torso.src" />
+            <img :src="selectedRobot.rightArm.src" class="rotate-right" />
+          </div>
+          <div class="bottom-row">
+            <img :src="selectedRobot.base.src" />
+          </div>
+        </div>
+      </CollapsibleSection>
       <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    </div>
     <div class="top-row">
       <!-- <div :class="[saleBorderClass, 'top', 'part']">
           <div class="robot-name">
               {{selectedRobot.head.title}}
               <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
           </div>
-      </div> -->
-      <PartSelector :parts="availableParts.heads"
-      position="top"/>
+      </div>-->
+      <PartSelector
+        :parts="availableParts.heads"
+        position="top"
+        @partSelected="part => selectedRobot.head=part"
+      />
     </div>
     <div class="middle-row">
-      <PartSelector :parts="availableParts.arms"
-      position="left"/>
-      <PartSelector :parts="availableParts.torsos"
-      position="center"/>
-      <PartSelector :parts="availableParts.arms"
-      position="right"/>
+      <PartSelector
+        :parts="availableParts.arms"
+        position="left"
+        @partSelected="part => selectedRobot.leftArm=part"
+      />
+      <PartSelector
+        :parts="availableParts.torsos"
+        position="center"
+        @partSelected="part => selectedRobot.torso=part"
+      />
+      <PartSelector
+        :parts="availableParts.arms"
+        position="right"
+        @partSelected="part => selectedRobot.rightArm=part"
+      />
     </div>
     <div class="bottom-row">
-      <PartSelector :parts="availableParts.bases"
-      position="bottom"/>
+      <PartSelector
+        :parts="availableParts.bases"
+        position="bottom"
+        @partSelected="part => selectedRobot.base=part"
+      />
     </div>
     <div>
-        <h1>Cart</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Robot</th>
-                    <th class="cost">Cost</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(robot, index) in cart" :key="index">
-                    <td>{{robot.head.title}}</td>
-                    <td class="cost">{{robot.cost}}</td>
-                </tr>
-            </tbody>
-        </table>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot, index) in cart" :key="index">
+            <td>{{robot.head.title}}</td>
+            <td class="cost">{{robot.cost}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -47,10 +79,11 @@
 import availableParts from '../data/parts';
 import createdHookMixin from './created-hook-mixin';
 import PartSelector from './PartSelector.vue';
+import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
-  components: { PartSelector },
+  components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
@@ -74,7 +107,10 @@ export default {
     addToCart() {
       const robot = this.selectedRobot;
       const cost = robot.head.cost
-       + robot.leftArm.cost + robot.torso.cost + robot.rightArm.cost + robot.base.cost;
+        + robot.leftArm.cost
+        + robot.torso.cost
+        + robot.rightArm.cost
+        + robot.base.cost;
       this.cart.push(Object.assign({}, robot, { cost }));
     },
   },
@@ -84,25 +120,25 @@ export default {
 <style lang="scss" scoped>
 .part {
   position: relative;
-  width:165px;
-  height:165px;
+  width: 165px;
+  height: 165px;
   border: 3px solid #aaa;
 }
 .part {
-    img {
-        width:165px;
-    }
+  img {
+    width: 165px;
+  }
 }
 .top-row {
-  display:flex;
+  display: flex;
   justify-content: space-around;
 }
 .middle-row {
-  display:flex;
+  display: flex;
   justify-content: center;
 }
 .bottom-row {
-  display:flex;
+  display: flex;
   justify-content: space-around;
   border-top: none;
 }
@@ -126,7 +162,7 @@ export default {
 }
 .prev-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   left: -28px;
   width: 25px;
@@ -134,14 +170,15 @@ export default {
 }
 .next-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   right: -28px;
   width: 25px;
   height: 171px;
 }
-.center .prev-selector, .center .next-selector {
-  opacity:0.8;
+.center .prev-selector,
+.center .next-selector {
+  opacity: 0.8;
 }
 .left .prev-selector {
   top: -28px;
@@ -173,33 +210,54 @@ export default {
   right: -3px;
 }
 .robot-name {
-    position: absolute;
-    top: -25px;
-    text-align: center;
-    width: 100%;
+  position: absolute;
+  top: -25px;
+  text-align: center;
+  width: 100%;
 }
 .sale {
-    color: red;
+  color: red;
 }
-.content{
-    position: relative;
+.content {
+  position: relative;
 }
-.add-to-cart{
-    position: absolute;
-    right: 30px;
-    width: 220px;
-    padding: 3px;
-    font-size: 16px;
+.add-to-cart {
+  position: absolute;
+  width: 210px;
+  padding: 3px;
+  font-size: 16px;
 }
-td, th{
-    text-align: left;
-    padding: 5px;
-    padding-right: 20px;
+td,
+th {
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
 }
-.cost{
-    text-align: right;
+.cost {
+  text-align: right;
 }
-.sale-border{
-border: 3px solid red;
+.sale-border {
+  border: 3px solid red;
+}
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
 }
 </style>
